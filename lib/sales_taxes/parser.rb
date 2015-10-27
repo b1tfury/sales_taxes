e3674c5c6670cbcccad2ac852356d27ea2348380
bitfurry <sahilkharb@iitj.ac.in>
2015-10-27 12:34:28 +0530

    Test case 3 : Parser#parse will give parsed values for a non imported item

--- lib/sales_taxes/parser.rb
+++ lib/sales_taxes/parser.rb
@@ -2,14 +2,35 @@ module SalesTaxes
   class Parser
     def initialize(input=nil)
       @input  = input
+      @item = Struct.new(:quantity,:imported,:good,:price)
     end
-
     def input
       if @input == nil
         raise "Nothing is passed to Parser"
       else
         return @input
       end
+    end
+
+    def parse
+      name_price = @input.split("at")
+      item = @item.new
+      item.price = name_price[1].to_f
+      product_details = name_price[0].split(" ")
+      item.quantity = product_details[0].to_i
+      good = ""
+      if product_details[1] == "imported"
+        item.imported = true
+        good = product_details[2,product_details.size]
+      else
+        item.imported = false
+        good = product_details[1,product_details.size]
+      end
+      item.good = ""
+      good.each do |i|
+        item.good += i
+      end
+      item
     end
   end
 end
--- spec/sales_taxes/parser_spec.rb
+++ spec/sales_taxes/parser_spec.rb
@@ -2,10 +2,18 @@ require 'spec_helper'
 module SalesTaxes
   describe Parser,"#input" do
     it "should raise error if now string is passed to Parser" do
-      expect{Parser.new()}.to raise_error(RuntimeError, 'Nothing is passed to Parser')
+      expect{Parser.new().input}.to raise_error(RuntimeError, 'Nothing is passed to Parser')
     end
     it "should return input string as output" do
       expect(Parser.new("1 book at 12.49").input).to eq("1 book at 12.49")
+    end
+  end
+
+  describe Parser,"#parse" do
+    it "should return the parsed values for a non imported item" do
+      item = Struct.new(:quantity,:imported,:good,:price)
+      op = item.new(1,false,"book",12.49)
+      expect(Parser.new("1 book at 12.49").parse).to have_attributes(:quantity => 1,:imported => false,:good => "book",:price => 12.49)
     end
   end
 end
