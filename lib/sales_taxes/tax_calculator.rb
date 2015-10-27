015d18fd53fc1f1debc60c1dadf6ec2c5c6e9dd6
bitfurry <sahilkharb@iitj.ac.in>
2015-10-27 14:02:57 +0530

    Test case 6 : TaxCalculator#calculate now returns tax of non imported good

--- lib/sales_taxes/tax_calculator.rb
+++ lib/sales_taxes/tax_calculator.rb
@@ -1,12 +1,24 @@
 module SalesTaxes
   class TaxCalculator
+    SALES_TAX = 0.10
+    IMPORT_DUTY = 0.05
     def initialize(item= nil)
       @item = item
+      @sales_tax = 0
     end
     def calculate
       if @item == nil
         raise "No input was given to calculate tax"
+      else
+        item_details = Parser.new(@item).parse
+        if !item_details.imported
+          @sales_tax = SALES_TAX*item_details.price
+        end
+        round_off(@sales_tax)
       end
+    end
+    def round_off(amount)
+        amount.round(2)
     end
   end
 end
--- spec/sales_taxes/tax_calculator_spec.rb
+++ spec/sales_taxes/tax_calculator_spec.rb
@@ -3,5 +3,8 @@ module SalesTaxes
     it "should raise error if something is not given to calculate tax" do
       expect{TaxCalculator.new.calculate}.to raise_error(RuntimeError,"No input was given to calculate tax")
     end
+    it "should calculate the tax of non imported good" do
+      expect(TaxCalculator.new("1 music CD at 14.99").calculate).to eq(1.50)
+    end
   end
 end
