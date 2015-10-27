ee11bd93850811cd32e4a028268fbee024e8ef42
bitfurry <sahilkharb@iitj.ac.in>
2015-10-27 12:53:47 +0530

    Test case 5 : TaxCalculator#calculate raise an error if nothing is passed to it.

--- lib/sales_taxes/tax_calculator.rb
+++ lib/sales_taxes/tax_calculator.rb
@@ -1,6 +1,13 @@
 module SalesTaxes
   class TaxCalculator
-
+    def initialize(item= nil)
+      @item = item
+    end
+    def calculate
+      if @item == nil
+        raise "No input was given to calculate tax"
+      end
+    end
   end
 end
 
--- spec/sales_taxes/tax_calculator_spec.rb
+++ spec/sales_taxes/tax_calculator_spec.rb
@@ -1,4 +1,7 @@
 module SalesTaxes
   describe TaxCalculator do
+    it "should raise error if something is not given to calculate tax" do
+      expect{TaxCalculator.new.calculate}.to raise_error(RuntimeError,"No input was given to calculate tax")
+    end
   end
 end
