8a076e464e2dc7093eb71c64aadbead9e4244e75
bitfurry <sahilkharb@iitj.ac.in>
2015-10-27 14:14:34 +0530

    Test Case 8 : TaxCalculator#calculate now returns 0 sales tax for books , food and medical products

--- spec/sales_taxes/tax_calculator_spec.rb
+++ spec/sales_taxes/tax_calculator_spec.rb
@@ -9,5 +9,8 @@ module SalesTaxes
     it "should calculate the tax of imported good" do
       expect(TaxCalculator.new("1 imported bottle of perfume at 27.99").calculate).to eq(4.20)
     end
+    it "should return 0 tax for books, food,and medical products" do
+      expect(TaxCalculator.new("1 book at 12.49").calculate).to eq(0)
+    end
   end
 end
